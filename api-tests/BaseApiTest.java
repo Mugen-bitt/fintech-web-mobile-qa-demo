4229f1146dd0cb6728c48b740a26e5c54f91bff1
Pavel <149588969+Mugen-bitt@users.noreply.github.com>
2025-11-03 10:44:36 +0300

    Update BaseApiTest.java

--- api-tests/BaseApiTest.java
+++ api-tests/BaseApiTest.java
@@ -1,1 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
 
+public class BaseApiTest {
+
+    protected static RequestSpecification reqSpec;
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+
+        reqSpec = new RequestSpecBuilder()
+                .setBaseUri(RestAssured.baseURI)
+                .setContentType("application/json")
+                .build();
+
+        RestAssured.requestSpecification = reqSpec;
+    }
+}
