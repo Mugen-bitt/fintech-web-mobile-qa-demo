740ee7d38096f2b3c4a306b57a0461177e8a44ee
Pavel <149588969+Mugen-bitt@users.noreply.github.com>
2025-11-09 18:16:34 +0300

    Update DataGenerator.java

--- framework/utils/DataGenerator.java
+++ framework/utils/DataGenerator.java
@@ -1,1 +1,14 @@
+package framework.utils;
 
+// Генератор тестовых данных (имена, телефоны, суммы и т.п.)
+public class DataGenerator {
+    public static String randomPhone() {
+        // Возвращает фиктивный номер телефона
+        return "+79001234567";
+    }
+
+    public static int randomLoanAmount() {
+        // Возвращает случайную сумму займа
+        return 10000;
+    }
+}
