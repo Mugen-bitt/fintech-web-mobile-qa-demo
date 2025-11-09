10a755d6a74fd377d1e35b28e75af62c96284963
Pavel <149588969+Mugen-bitt@users.noreply.github.com>
2025-11-09 18:12:14 +0300

    Update LoginPage.java

--- framework/pages/LoginPage.java
+++ framework/pages/LoginPage.java
@@ -1,1 +1,14 @@
+package framework.pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+// Page Object для авторизации пользователя
+public class LoginPage {
+    private SelenideElement phoneField;
+    private SelenideElement passwordField;
+    private SelenideElement loginButton;
+
+    public void login(String phone, String password) {
+        // Ввод данных и нажатие кнопки входа
+    }
+}
