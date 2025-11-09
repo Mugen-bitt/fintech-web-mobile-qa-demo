d321f99a313bbe11082b41c135db11f458aa684a
Pavel <149588969+Mugen-bitt@users.noreply.github.com>
2025-11-09 18:12:48 +0300

    Update LoanApi.java

--- framework/api/LoanApi.java
+++ framework/api/LoanApi.java
@@ -1,1 +1,11 @@
+package framework.api;
 
+import io.restassured.response.Response;
+
+// Класс для работы с API заявок на займ
+public class LoanApi {
+    public Response createLoan(String userId, int amount) {
+        // POST /loan
+        return null;
+    }
+}
