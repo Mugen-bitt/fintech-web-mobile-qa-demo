0bd88edbb1a7f2562e67d131594cdee849caae0d
Pavel <149588969+Mugen-bitt@users.noreply.github.com>
2025-11-09 18:17:08 +0300

    Update LoanData.java

--- framework/data/LoanData.java
+++ framework/data/LoanData.java
@@ -1,1 +1,25 @@
+package framework.data;
 
+// Data Builder для объекта заявки на займ
+public class LoanData {
+    private String userId;
+    private int amount;
+
+    public LoanData withUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public LoanData withAmount(int amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
